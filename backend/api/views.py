8f38cfeabe8dcb818aa54e7709c16bcffdd391b4
Tony Pérez <tonper19@gmail.com>
2025-08-07 16:05:27 +0100

    Echo_GET_Data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,11 @@
 from django.http import JsonResponse
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello, world! this is your Django API response!"})
+  body = request.body
+  data = {}
+  try:
+    data = json.loads(body)
+  except:
+    pass
+  print(data.keys())
+  return JsonResponse({"message": "Hello, world! this is your Django API response (views.py)!"})
