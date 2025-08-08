df8a3346c8e1f188fcd8128afdde672d96984b82
Tony Pérez <tonper19@gmail.com>
2025-08-08 12:35:40 +0100

    Injest_Data_with_Django_Rest_Framework_Views

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -7,8 +7,13 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['title', 'content', 'price', 'sale_price', 'your_discount']
         # read_only_fields = ['id', 'sale_price']
-        
+
     def get_your_discount(self, obj):
+        if not hasattr(obj, 'get_discount'):
+            return None
+        if not isinstance(obj, Product):
+            return None
+            
         return obj.get_discount()
 
 
